33b163002bb59896290a23ca6e20c6081730c922
ahassid <33699232+ahassid@users.noreply.github.com>
2023-02-19 16:34:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import Printer
 import random
 import Game
 
-wordsArray = ["hello", "crate", "shade", "resin", "alert", "media", "flood", "award", "bully", "orate", "legal", "boost"]
-G = Game.Game()
+G = Game.Game("Words.txt")
+#wordsArray = ["hello", "crate", "shade", "resin", "alert", "media", "flood", "award", "bully", "orate", "legal", "boost"]
 
 while True:
-    realWord = wordsArray [random.randint(0, 11)]
-    print ("The Real Word is: " + realWord)
     
     try:
-        color = input ("Enter 1 if you want colors in your game, Else enter 0: ")
-        if G.startGame(realWord, int(color)) == 0:
+        color = int(input ("Enter 1 if you want colors in your game, Else enter 0: "))
+        chosenWord = G.arrayOfWords [random.randint(0, 11)]
+        print ("The Real Word is: " + chosenWord)
+        if G.StartGame(chosenWord, int(color)) == 0:
             break
     except ValueError:
         print("Sorry ! You Entered An Inappropriate Value ")
-                    
-    
