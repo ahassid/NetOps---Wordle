1238401f367fd85921a936ae84247698b9715531
ahassid <33699232+ahassid@users.noreply.github.com>
2023-02-19 16:34:21 +0200

    Update Printer.py

--- Printer.py
+++ Printer.py
@@ -1,24 +1,28 @@
-import Bcolors
 
 class Printer:
-      word = ""
       
-def S_Print(word, result ,_type):
-    bc = Bcolors.Bcolors()
-    i = 0
-    _str = ""
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    WHITE = '\033[97m' 
+
+    def ColourfulPrint(self, word, result ,_type):
+        i = 0
+        _str = ""
     
-    if _type == 1:
-        while i < 5:
-            for r in result:
-                if r == "F":
-                    _str += bc.RED + word[i]
-                elif r == "T":
-                    _str += bc.GREEN + word[i]
-                else:
-                    _str += bc.YELLOW + word[i]
-                i += 1
-        print (_str + bc.WHITE)        
-    else:    
-        print (result)
+        if _type == 1:
+            while i < 5:
+                for r in result:
+                    if r == "F":
+                        _str += self.RED + word[i]
+                    elif r == "T":
+                        _str += self.GREEN + word[i]
+                    else:
+                        _str += self.YELLOW + word[i]
+                    i += 1
+            print (_str + self.WHITE)        
+        else:    
+            print (result)
+  
+  
   
